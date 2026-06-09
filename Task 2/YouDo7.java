1fd8d70385c4f457e63f50a92d057b47f122dea6
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-06-09 10:57:39 +0600

    Update YouDo7.java

--- Task 2/YouDo7.java
+++ Task 2/YouDo7.java
@@ -1,1 +1,19 @@
 
+
+interface clickable {
+
+    void onClick();
+}
+
+class Button implements clickable{
+    public void onClick(){
+        System.out.println("Button was clicked");
+    }
+}
+
+public class main7 {
+    public static void main(String[] args) {
+        Button myButton = new Button();
+        myButton.onClick();
+    }
+}
