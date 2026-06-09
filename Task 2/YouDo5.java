177598f1d5ebec19a25a6f8217efbc910d4440a8
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-06-09 10:39:56 +0600

    Update YouDo5.java

--- Task 2/YouDo5.java
+++ Task 2/YouDo5.java
@@ -1,1 +1,19 @@
 
+class Printer{
+    public void printData(String text){
+        System.out.println("Printing text: " + text);
+    }
+
+    public void printData(int number){
+        System.out.println("Printing number: " + number);
+    }
+}
+
+
+public class main5 {
+    public static void main(String[] args) {
+        Printer myPrinter = new Printer();
+        myPrinter.printData("Hello, World!");
+        myPrinter.printData(42);
+    }
+}
