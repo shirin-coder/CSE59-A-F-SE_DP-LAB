cd8d03782d4657c3ca6209ec0dfc0d2422d0bbfe
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-06-30 10:35:49 +0600

    Update Main4.java

--- Task 5/Main4.java
+++ Task 5/Main4.java
@@ -1,1 +1,34 @@
 
+interface OptimizableModel{
+    void quantize();
+}
+
+class ResNet18 implements OptimizableModel{
+    public void quantize(){
+        System.out.println("Applying int8 quantization for ResNet-18.");
+    }
+}
+
+class MobileNet implements OptimizableModel{
+    public void quantize(){
+        System.out.println("Applying dynamic quantization for MobileNet.");
+    }
+}
+
+class InferenceEngine{
+    public void OptimizableModel(OptimizableModel model){
+        model.quantize();
+    }
+}
+
+public class Main4{
+    public static void main(String[] args) {
+        InferenceEngine engine = new InferenceEngine();
+        OptimizableModel resnet = new ResNet18();
+        engine.OptimizableModel(resnet);
+        OptimizableModel mobile = new ResNet18();
+        engine.OptimizableModel(mobile);
+        
+        
+    }
+}
