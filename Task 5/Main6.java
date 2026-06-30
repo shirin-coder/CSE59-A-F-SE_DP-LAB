27285e2ea8c4562aeea50d51898715ab6d6a8234
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-06-30 10:57:12 +0600

    Implement HTTP, Database, and Docker management services

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,44 @@
+import javax.xml.crypto.Data;
 
+interface HttpService {
+    void handleHttpRequest();
+}
+
+interface DatabaseManager {
+    void executeSqlStatement();
+}
+
+interface ContainerOps {
+    void restartDockerContainer();
+}
+
+class WebController implements HttpService{
+    public void handleHttpRequest(){
+        System.out.println("Routing traffic to endpoint.");
+    }
+}
+
+class DatabaseService implements DatabaseManager{
+    public void executeSqlStatement(){
+        System.out.println("Executing SQL statement.");
+    }
+}
+
+class DockerManager implements ContainerOps {
+    public void restartDockerContainer(){
+        System.out.println("Restarting Docker container.");
+    }
+}
+
+public class Main6{
+    public static void main(String[] args) {
+        HttpService controller = new WebController();
+        controller.handleHttpRequest();
+
+        DatabaseManager db = new DatabaseService();
+        db.executeSqlStatement();
+
+        ContainerOps docker = new DockerManager();
+        docker.restartDockerContainer();
+    }
+}
