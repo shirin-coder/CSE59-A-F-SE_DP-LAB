0c663c87a6ab05907d8a7c7f05c08c76460b90eb
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-06-30 11:06:07 +0600

    Update Main7.java

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,39 @@
 
+interface INetworkProtocol {
+    void broadcastMessage(String msg);
+}
+
+class BluetoothMeshNetwork implements INetworkProtocol{
+    public void broadcastMessage(String msg){
+        System.out.println("Broadcasting via Bluetooth Mesh: " + msg);
+    }
+}
+
+class WiFiDirectNetwork implements INetworkProtocol{
+    public void broadcastMessage(String msg){
+        System.out.println("Broadcasting via WiFi Direct: " + msg);
+    }
+}
+
+class DisasterAlertSystem{
+    private INetworkProtocol network;
+    public DisasterAlertSystem(INetworkProtocol network){
+        this.network = network;
+    }
+
+    public void triggerAlert(String alertData){
+        network.broadcastMessage(alertData);
+    }
+}
+
+public class Main7{
+    public static void main(String[] args) {
+        INetworkProtocol bluetooth = new BluetoothMeshNetwork();
+        DisasterAlertSystem alert1 = new DisasterAlertSystem(bluetooth);
+        alert1.triggerAlert("Earthquake detected!");
+
+        INetworkProtocol wifi = new WiFiDirectNetwork();
+        DisasterAlertSystem alert2 = new DisasterAlertSystem(wifi);
+        alert2.triggerAlert(("Flood warning!"));
+    }
+}
