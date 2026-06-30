2ce54e75881984d96722aebc7fb4d369a6ab3151
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-06-30 10:46:08 +0600

    Update Main5.java

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,41 @@
 
+interface Researcher{
+    void conductLabResearch();
+}
+
+class UniversityMember {
+    public void attendLecture(){
+        System.out.println("Attending a lecture.");
+    }
+}
+
+class UndergraduateStudent extends UniversityMember{
+    // no research responsibilities
+}
+
+class GraduateResearchAssistant extends UniversityMember implements Researcher{
+    public void conductLabResearch(){
+        System.out.println("Publishing papers and running experiments.");
+    }
+}
+
+class Professor extends UniversityMember implements Researcher{
+    public void conductLabResearch(){
+        System.out.println("Publishing papers and running experiments.");
+    }
+}
+
+public class Main5{
+    public static void main(String[] args) {
+        UndergraduateStudent student = new UndergraduateStudent();
+        student.attendLecture();
+
+        GraduateResearchAssistant gra = new GraduateResearchAssistant();
+        gra.attendLecture();
+        gra.conductLabResearch();
+
+        Professor professor = new Professor();
+        professor.attendLecture();
+        professor.conductLabResearch();
+    }
+}
