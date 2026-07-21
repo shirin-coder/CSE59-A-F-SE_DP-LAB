6f71e2c4978301a6be6eb6f22750e836fbc36fdd
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-07-21 11:24:00 +0600

    Update Main4.java

--- Task 8/Main4.java
+++ Task 8/Main4.java
@@ -1,1 +1,30 @@
 
+class ConfigurationManager {
+    private static volatile ConfigurationManager instance;
+
+    private ConfigurationManager (){
+        System.out.println("Reading heavy config file from disk... (Only happens once)");
+
+    }
+
+    public static ConfigurationManager getInstance(){
+        if(instance == null){
+            synchronized(ConfigurationManager.class){
+                if(instance == null){
+                    instance = new ConfigurationManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
+
+public class main4 {
+    public static void main(String[] args) {
+        ConfigurationManager config1 = ConfigurationManager.getInstance();
+        ConfigurationManager config2 = ConfigurationManager.getInstance();
+        
+        System.out.println("Same instance in memory? " + (config1 == config2)); // Output: true
+    }
+}
