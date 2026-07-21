c2f67bc2a300d97c2d40a729909f6cded539e586
Rabeya Fatema Shirin <159103871+shirin-coder@users.noreply.github.com>
2026-07-21 11:08:20 +0600

    Implement SqlQuery class with Builder pattern

--- Task 8/Main3.java
+++ Task 8/Main3.java
@@ -1,1 +1,41 @@
 
+class SqlQuery{
+    private final String query;
+    private SqlQuery(Builder builder){
+        this.query = "SELECT" + builder.select + " FROM " + builder.from +
+                    (builder.where != null ? " WHERE " + builder.where : ";");
+
+
+    }
+
+    public String getQuery(){
+        return query;
+    }
+
+    public static class Builder {
+        private String select = "*";
+        private String from;
+        private String where;
+
+        public Builder from(String table) { this.from = table; return this; }
+        public Builder select(String columns) { this.select = columns; return this; }
+        public Builder where(String condition) { this.where = condition; return this; }
+
+        public SqlQuery build() {
+            if (this.from == null) throw new IllegalStateException("Error: FROM required.");
+            return new SqlQuery(this);
+        }
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+            SqlQuery query = new SqlQuery.Builder()
+                                 .select("name, registration_num")
+                                 .from("students")
+                                 .where("batch >= 20")
+                                 .build();
+                                 
+            System.out.println(query.getQuery());
+        }
+    }
+}
